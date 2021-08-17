9d17d0a6fe022557c41f1908b7ff6b396fd74400
jsun <jaysun054@gmail.com>
2021-08-16 23:59:20 -0700

    inital game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,7 +13,6 @@ var fight = function() {
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     
     if (promptFight === "fight" || promptFight === "FIGHT") {
-        // remove enemy's health by subtracting the amount set in the playerAttack variable
         enemyHealth = enemyHealth - playerAttack;
         console.log(
           playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
@@ -25,14 +24,11 @@ var fight = function() {
         } else {
           window.alert(enemyName + " still has " + enemyHealth + " health left.");
         }
-      
-        // remove player's health by subtracting the amount set in the enemyAttack variable
+
         playerHealth = playerHealth - enemyAttack;
         console.log(
           enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
         );
-      
-        // check player's health
         if (playerHealth <= 0) {
           window.alert(playerName + " has died!");
         } else {
@@ -40,21 +36,17 @@ var fight = function() {
         }
         // if player choses to skip
       } else if (promptFight === "skip" || promptFight === "SKIP") {
-    
-        // confirm player wants to skip
         var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
         // if yes (true), leave fight
         if (confirmSkip) {
         window.alert(playerName + " has decided to skip this fight. Goodbye!");
-        // subtract money from playerMoney for skipping
         playerMoney = playerMoney - 2;
         }
         // if no (false), ask question again by running fight() again
         else {
         fight();
         }
-        
+
       } else {
         window.alert("You need to choose a valid option. Try again!");
       }
